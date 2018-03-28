edcb944ce01a3464d27b690edbe2756e4f70d5ad
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2018-03-28 07:55:00 +0200

    Python: Reverse String - Simplified solution

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,11 +1,5 @@
 def reverse(input=''):
     if not isinstance(input, str):
         raise Exception("ValueError: input should be of type str")
-    
-    reverse = ''
-    count = len(input)
-
-    for index in range(count - 1, -1, -1):
-        reverse += input[index]
 
     return input[::-1]
