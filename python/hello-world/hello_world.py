433330521aa376065c16f8f491881b424ccf7597
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2018-03-28 07:35:57 +0200

    Checking parameter type

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,2 +1,5 @@
 def hello(name='World'):
+    if not isinstance(name, str):
+        raise Exception("ValueError: name should be of type str")
+
     return 'Hello, {}!'.format(name)
--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,5 @@
 def is_leap_year(year):
+    if not isinstance(year, int):
+        raise Exception("ValueError: year should be of type int")
+
     return (False, (True, year % 400 == 0)[year % 100 == 0])[year % 4 == 0]
