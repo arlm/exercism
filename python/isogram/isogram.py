e8f7ad889059456694b797312e3cfcc47c2e736a
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-04-04 15:13:43 +0200

    Python: Isogram - Better for each

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,11 @@
 def is_isogram(string):
-    for c in string.lower():
-        if string.lower().count(c) > 1 and c != ' ' and c != '-':
+    if not isinstance(string, str):
+        raise Exception("ValueError: string should be of type str")
+
+    if string == "":
+        return True
+
+    for letter in [c for c in string.lower() if c.isalpha()]:
+        if string.lower().count(letter) > 1:
             return False
     return True
