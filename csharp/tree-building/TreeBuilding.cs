cc97a6b0ad3acf5ad00b897c685a528b503bad3f
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-09-15 19:11:12 +0200

    Extracting validation method and simplifying if clauses

--- csharp/tree-building/TreeBuilding.cs
+++ csharp/tree-building/TreeBuilding.cs
@@ -31,20 +31,15 @@ public static class TreeBuilder
         var previousRecordId = -1;
 
         foreach (var record in records)
-        {   
+        {
             var t = new Tree { Children = new List<Tree>(), Id = record.RecordId, ParentId = record.ParentId };
             trees.Add(t);
 
-            if ((t.Id == 0 && t.ParentId != 0) ||
-                (t.Id != 0 && t.ParentId >= t.Id) ||
-                (t.Id != 0 && t.Id != previousRecordId + 1))
-            {
-                throw new ArgumentException();
-            }
+            ValidateRecord(previousRecordId, t);
 
             previousRecordId++;
         }
-        
+
         if (trees.Count == 0)
         {
             throw new ArgumentException();
@@ -57,7 +52,19 @@ public static class TreeBuilder
             parent.Children.Add(t);
         }
 
-        var r = trees.First(t => t.Id == 0);
-        return r;
+        return trees.First(t => t.Id == 0);
+    }
+
+    private static void ValidateRecord(int previousRecordId, Tree t)
+    {
+        if (t.Id == 0 && (t.ParentId != 0))
+        {
+            throw new ArgumentException();
+        }
+
+        if (t.Id != 0 && (t.ParentId >= t.Id || t.Id != previousRecordId + 1))
+        {
+            throw new ArgumentException();
+        }
     }
 }
