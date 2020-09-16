2d79bec081447c7d434d7028207577ecae1b5bba
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-09-16 22:01:35 +0200

    Using extension methods to simplify the calls.

--- csharp/markdown/Markdown.cs
+++ csharp/markdown/Markdown.cs
@@ -15,40 +15,31 @@ public static class Markdown
     private const string MARKDOWN_BOLD = "__";
     private const string MARKDOWN_ITALIC = "_";
 
-    private static string Wrap(string text, string tag) =>
+    private static string Wrap(this string text, string tag) =>
         $"<{tag}>{text}</{tag}>";
 
-    private static string Parse(string markdown, string delimiter, string tag) =>
+    private static string Parse(this string markdown, string delimiter, string tag) =>
         Regex.Replace(markdown, $"{delimiter}(.+){delimiter}", $"<{tag}>$1</{tag}>");
-
-    private static string ParseBold(string markdown) =>
-        Parse(markdown, MARKDOWN_BOLD, HTML_BOLD);
-
-    private static string ParseItalic(string markdown) =>
-        Parse(markdown, MARKDOWN_ITALIC, HTML_ITALIC);
-
-    private static string ParseParagraph(string markdown) =>
-        Wrap(markdown, HTML_PARAGRAPH);
 
     private static string ParseHeader(string markdown)
     {
         var count = markdown.TakeWhile(@char => @char == MARKDOWN_HEADER).Count();
-        var headerHtml = Wrap(markdown.Substring(count + 1), $"h{count}");
+        var headerHtml = markdown.Substring(count + 1).Wrap($"h{count}");
 
         return headerHtml;
     }
 
     private static string ParseLineItem(string markdown, bool list)
     {
-        var innerHtml = Wrap(markdown.Substring(2), HTML_LIST);
+        var innerHtml = markdown.Substring(2).Wrap(HTML_LIST);
 
         return list ? innerHtml : $"<ul>{innerHtml}";
     }
     
     private static (string, bool) ParseLine(string markdown, bool list)
     {
-        markdown = ParseBold(markdown);
-        markdown = ParseItalic(markdown);
+        markdown = markdown.Parse(MARKDOWN_BOLD, HTML_BOLD);
+        markdown = markdown.Parse(MARKDOWN_ITALIC, HTML_ITALIC);
 
         string result = null;
         bool isInList = list;
@@ -68,7 +59,7 @@ public static class Markdown
 
         if (result == null)
         {
-            var value = ParseParagraph(markdown);
+            var value = markdown.Wrap(HTML_PARAGRAPH);
             result = isInList ? $"</ul>{value}" : value;
             isInList = false;
         }
