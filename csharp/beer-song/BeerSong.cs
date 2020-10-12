33c59dd9baed1f481563fd2e8e7d0d31a11f32fb
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-10-12 19:21:30 +0200

    Better StringBuilder appending methods and logic flow

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -9,56 +9,58 @@ public static class BeerSong
 
         for (int count = startBottles; takeDown > 0; takeDown--, count--)
         {
-            if (count > 2)
-            {
-                Verse(count, sb);
-            }
-            else if (count == 2)
-            {
-                VerseTwo(sb);
-            }
-            else if (count == 1)
-            {
-                VerseOne(sb);
-            }
-            else
-            {
-                VerseZero(sb);
-            }
+            sb.AppendFirstVerse(count);
+            sb.AppendSecondVerse(count);
 
             if (takeDown > 1)
             {
-                sb.AppendLine(string.Empty);
-                sb.AppendLine(string.Empty);
+                sb  
+                    .AppendNewLine()
+                    .AppendNewLine();
             }
         }
 
         return sb.ToString();
     }
 
-    private static void Verse(int startBottles, StringBuilder sb)
-    {
-        sb.AppendFormat("{0} bottles of beer on the wall, {0} bottles of beer.", startBottles);
-        sb.AppendLine(string.Empty);
-        startBottles--;
-        sb.AppendFormat("Take one down and pass it around, {0} bottles of beer on the wall.", startBottles);
-    }
+    private static StringBuilder AppendNumberOfBottles(this StringBuilder sb, int startBottles, bool beginSentence) =>
+        startBottles > 0 ?
+            sb.Append(startBottles)
+        :
+            beginSentence ?
+                sb.Append("No more")
+            :
+                sb.Append("no more");
 
-    private static void VerseTwo(StringBuilder sb)
-    {
-        sb.AppendLine("2 bottles of beer on the wall, 2 bottles of beer.");
-        sb.Append("Take one down and pass it around, 1 bottle of beer on the wall.");
-    }
+    private static StringBuilder AppendBottlesOfBeer(this StringBuilder sb, int startBottles) => sb
+        .Append(startBottles == 1 ? " bottle" : " bottles")
+        .Append(" of beer");
 
-    private static void VerseOne(StringBuilder sb)
-    {
-        sb.AppendLine("1 bottle of beer on the wall, 1 bottle of beer.");
-        sb.Append("Take it down and pass it around, no more bottles of beer on the wall.");
-    }
+    private static StringBuilder AppendOneOrIt(this StringBuilder sb, int startBottles) => sb
+       .Append(startBottles == 1 ? "it" : "one");
 
-    private static void VerseZero(StringBuilder sb)
-    {
-        sb.AppendLine("No more bottles of beer on the wall, no more bottles of beer.");
-        sb.Append("Go to the store and buy some more, 99 bottles of beer on the wall.");
-    }
+    private static StringBuilder AppendNewLine(this StringBuilder sb) => sb
+        .Append("\n");
+
+    private static StringBuilder AppendFirstVerse(this StringBuilder sb, int startBottles) => sb
+        .AppendNumberOfBottles(startBottles, true)
+        .AppendBottlesOfBeer(startBottles)
+        .AppendFormat(" on the wall, ")
+        .AppendNumberOfBottles(startBottles, false)
+        .AppendBottlesOfBeer(startBottles)
+        .Append(".")
+        .AppendNewLine();
+
+    private static StringBuilder AppendSecondVerse(this StringBuilder sb, int startBottles) =>
+        startBottles == 0 ?
+            sb
+            .Append("Go to the store and buy some more, 99 bottles of beer on the wall.")
+        :
+            sb        
+            .AppendFormat("Take ")
+            .AppendOneOrIt(startBottles)
+            .Append(" down and pass it around, ")
+            .AppendNumberOfBottles(--startBottles, false)
+            .AppendBottlesOfBeer(startBottles)
+            .AppendFormat(" on the wall.");
 }
