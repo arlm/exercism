4820a20ebfe56e9eca7170646bfc730b473d305b
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-10-27 10:40:27 +0100

    Comparing utf8 runes instead of bytes on the Go hammer exercise.

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,6 +1,9 @@
 package hamming
 
-import "errors"
+import (
+	"errors"
+	"unicode/utf8"
+)
 
 // Distance calculates the Hamming distance of two DNA strands
 func Distance(a, b string) (int, error) {
@@ -9,10 +12,13 @@ func Distance(a, b string) (int, error) {
 	}
 
 	count := 0
-	len := len(a)
 
-	for index := 0; index < len; index++ {
-		if a[index] != b[index] {
+	for index, width := 0, 0; index < len(a); index += width {
+		aRuneValue, aWidth := utf8.DecodeRuneInString(a[index:])
+		bRuneValue, bWidth := utf8.DecodeRuneInString(b[index:])
+		width = aWidth
+
+		if aRuneValue != bRuneValue || aWidth != bWidth {
 			count++
 		}
 	}
