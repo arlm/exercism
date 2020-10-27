f0d8d693d67ec6073e1b9d2fd9bd2876e125c662
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-10-27 18:28:27 +0100

    Regex solutions for RNA Transcription on Go an Lua

--- lua/rna-transcription/rna-transcription.lua
+++ lua/rna-transcription/rna-transcription.lua
@@ -1,3 +1,9 @@
 return function(dna)
+    local result = dna:gsub("A", "U")
+    result = result:gsub("T", "A")
+    result = result:gsub("G", "X")
+    result = result:gsub("C", "G")
+    result = result:gsub("X", "C")
 
+    return result
 end
