2867d27158b370cfdb5f99ea25f135b1278c8d09
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-10-29 14:42:27 +0100

    Simplified the proverb Go solution

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,27 +1,26 @@
 // Package proverb provides a proverbial sequence of sentences given a list of items.
 package proverb
 
-import (
-	"fmt"
+import "fmt"
+
+const (
+	stanza = "For want of a %s the %s was lost."
+	last   = "And all for the want of a %s."
 )
 
 // Proverb generates a sequence of proverbial sentences using the given items.
 func Proverb(rhyme []string) []string {
-	result := []string{}
 	count := len(rhyme)
+	result := make([]string, count)
 
-	for index := 0; index < count; index++ {
-		var sentence string
-
-		if index == count-1 {
-			sentence = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+	for index := range rhyme {
+		if index+1 == count {
+			result[index] = fmt.Sprintf(last, rhyme[0])
 		} else {
 			item1 := rhyme[index]
 			item2 := rhyme[index+1]
-			sentence = fmt.Sprintf("For want of a %s the %s was lost.", item1, item2)
+			result[index] = fmt.Sprintf(stanza, item1, item2)
 		}
-
-		result = append(result, sentence)
 	}
 
 	return result
