adaf0a5bd4294b7b41c1e50a67a1847d18bfc770
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2021-09-20 16:59:39 +0200

    Solving C# Log Levels

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -2,18 +2,9 @@ using System;
 
 static class LogLine
 {
-    public static string Message(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
+    public static string Message(string logLine) => logLine.Substring(logLine.IndexOf(':') + 1).Trim();
 
-    public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+    public static string LogLevel(string logLine) => logLine.Substring(1, logLine.IndexOf(']') - 1).ToLower();
 
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
+    public static string Reformat(string logLine) => string.Format("{0} ({1})", Message(logLine), LogLevel(logLine));
 }
