2f95d9ee478a68e08e2c7b93cfe310d0c5801b70
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2021-09-20 20:50:32 +0200

    Solution for C# exercise Elon's Toys

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -1,24 +1,23 @@
-using System;
-
 class RemoteControlCar
 {
-    public static RemoteControlCar Buy()
-    {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
-    }
+    private int batteryLevel = 100;
+    private int distance = 0;
 
-    public string DistanceDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
-    }
+    public static RemoteControlCar Buy() => new RemoteControlCar();
 
-    public string BatteryDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
-    }
+    public string DistanceDisplay() => $"Driven {distance} meters";
+
+    public string BatteryDisplay() => batteryLevel == 0
+        ? "Battery empty"
+        : $"Battery at {batteryLevel}%";
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (batteryLevel == 0) {
+            return;
+        }
+
+        batteryLevel--;
+        distance += 20;
     }
 }
