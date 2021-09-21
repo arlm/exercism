f28f9b5986fd1cb3847cb421a6381790970aa12e
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2021-09-21 23:19:48 +0200

    Solution for C# exercises: * Bord Watcher * Booking up for Beatuy

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -2,28 +2,18 @@ using System;
 
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
-    }
+    public static DateTime Schedule(string appointmentDateDescription) =>
+        DateTime.Parse(appointmentDateDescription);
 
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasPassed() method");
-    }
+    public static bool HasPassed(DateTime appointmentDate) =>
+        DateTime.Now > appointmentDate;
 
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.IsAfternoonAppointment() method");
-    }
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) =>
+        appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+    public static string Description(DateTime appointmentDate) =>
+        $"You have an appointment on {appointmentDate:G}.";
 
-    public static DateTime AnniversaryDate()
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
-    }
+    public static DateTime AnniversaryDate() =>
+        new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
 }
