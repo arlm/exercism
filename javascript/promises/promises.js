f8a99c21ab209527d5b57c4f547cf4bd82a40ec0
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2021-10-04 22:44:01 +0200

    Solution for Javascript exercise Promises

--- javascript/promises/promises.js
+++ javascript/promises/promises.js
@@ -3,22 +3,74 @@
 // convenience to get you started writing code faster.
 //
 
-export const promisify = () => {
-  throw new Error('Remove this statement and implement this function');
+export const promisify = (callbackFunction) => {
+  return (parameter) => {
+    return new Promise((resolve, reject) => {
+      callbackFunction(parameter, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
 };
 
-export const all = () => {
-  throw new Error('Remove this statement and implement this function');
+export const all = async (promises) => {
+  try {
+    const result = [];
+
+    for (const promise of promises) {
+      result.push(await promise);
+    }
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const allSettled = () => {
-  throw new Error('Remove this statement and implement this function');
+export const allSettled = async (promises) => {
+  const result = [];
+
+  for (const promise of promises) {
+    try {
+      result.push(await promise);
+    } catch (error) {
+      result.push(error);
+    }
+  }
+
+  return result;
 };
 
-export const race = () => {
-  throw new Error('Remove this statement and implement this function');
+export const race = (promises) => {
+  return new Promise((resolve, reject) => {
+    for (const promise of promises) {
+      promise
+        .then((result) => resolve(result))
+        .catch((error) => reject(error));
+    }
+  });
 };
 
-export const any = () => {
-  throw new Error('Remove this statement and implement this function');
+export const any = (promises) => {
+  return new Promise(async (resolve, reject) => {
+    const errors = [];
+
+    for (const promise of promises) {
+      promise
+        .then(
+          result => resolve(result),
+          error => errors.push(error)
+        );
+    }
+
+    while (errors.length < promises.length) {
+      await new Promise(resolve => setTimeout(resolve, 150));
+    }
+
+    reject(errors);
+  });
 };
