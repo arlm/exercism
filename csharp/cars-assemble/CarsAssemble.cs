389144a5a742ae1483d41d0e0acdd2c2de3d8a56
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2023-12-04 18:02:29 +0100

    Updating C# exercises

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -4,20 +4,19 @@ static class AssemblyLine
 {
     private const int RATE_PER_HOUR = 221;
 
-    public static double ProductionRatePerHour(int speed)
-    {
-
-        return RATE_PER_HOUR
-            * speed
-            * speed switch
-            {
+    public static double SuccessRate(int speed) =>
+        speed switch
+        {
                 >= 1 and <= 4 => 1,
                 >= 5 and <= 8 => 0.9,
                 9 => 0.8,
                 10 => 0.77,
                 _ => 0.0,
-            };
-    }
+        };
+    
+    public static double ProductionRatePerHour(int speed) =>
+        RATE_PER_HOUR * speed * SuccessRate(speed);
 
-    public static int WorkingItemsPerMinute(int speed) => (int)Math.Floor(ProductionRatePerHour(speed) / 60);
+    public static int WorkingItemsPerMinute(int speed) =>
+        (int)Math.Floor(ProductionRatePerHour(speed) / 60);
 }
