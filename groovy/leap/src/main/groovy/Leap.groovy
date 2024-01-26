a7148fd7d1f0c49257c4a24e3d22435349eed0a2
Alexandre Marcondes <alexandre.marcondes@docuware.com>
2024-01-26 09:54:50 +0100

    Clojure, Groovy, Java, Kotlin, Scala exercises

--- groovy/leap/src/main/groovy/Leap.groovy
+++ groovy/leap/src/main/groovy/Leap.groovy
@@ -1,11 +1,18 @@
+// This class represents a year and determines if it is a leap year or not
 class Leap {
+    Integer year
 
+    // The constructor takes a year as a parameter and assigns it to the instance field
     Leap(Integer year) {
-        throw new UnsupportedOperationException('Constructor implementation is missing')
+        this.year = year
     }
 
+    // This method returns true if the year is a leap year, false otherwise
+    // A leap year occurs:
+    // - In every year that is evenly divisible by 4
+    // - Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400
     def isLeapYear() {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        year % 100 == 0 ? year % 400 == 0 : year % 4 == 0
     }
 
 }
---
+++ kotlin/.devcontainer/postCreateCommand.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sdk install java 11.0.22-tem
+sdk default java 21.0.2-tem
+
+cat << EOF
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.'
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+EOF >"$HOME/.zshrc-tmp"
+
+cat "$HOME/.zshrc" >> "$HOME/.zshrc-tmp"
+
+cat << EOF
+
+# To customize prompt, run $(p10k configure) or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"
+
+EOF >>"$HOME/.zshrc-tmp"
+
+cp  "$HOME/.zshrc-tmp" "$HOME/.zshrc"
+rm "$HOME/.zshrc-tmp"
+
+mkdir -p "$HOME/.config/exercism"
+cp "$HOME/.exercism-config/user.json" "$HOME/.config/exercism/user.json"
+exercism configure -w /workspaces/exercism
+
+curl https://rakubrew.org/install-on-perl.sh | sh
+echo 'eval "$(/home/vscode/.rakubrew/bin/rakubrew init Zsh)"' > "$HOME/.zshenv"
+rakubrew build-zef
+
+git clone https://github.com/ugexe/zef.git
+cd zef
+raku -I. bin/zef install .
+cd -
+
+echo 'export PATH=$PATH:$HOME/zef/bin' >> "$HOME/.zshrc"
+
+$HOME/zef/bin/zef
+
+set +ex
+source "$HOME/.zshrc"
+set -ex
+
+omz theme set powerlevel10k/powerlevel10k
