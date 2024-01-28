fbc53ab62838538eb3888c78ea7dfe347bfccd5a
Alexandre Marcondes <alexandre.marcondes@docuware.com>
2024-01-28 19:10:30 +0100

    Updating devcontainer

--- kotlin/.devcontainer/postCreateCommand.sh
+++ kotlin/.devcontainer/postCreateCommand.sh
@@ -7,28 +7,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 sdk install java 11.0.22-tem
 sdk default java 21.0.2-tem
 
-cat << EOF
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.'
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-EOF >"$HOME/.zshrc-tmp"
-
-cat "$HOME/.zshrc" >> "$HOME/.zshrc-tmp"
-
-cat << EOF
-
-# To customize prompt, run $(p10k configure) or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"
-
-EOF >>"$HOME/.zshrc-tmp"
-
-cp  "$HOME/.zshrc-tmp" "$HOME/.zshrc"
-rm "$HOME/.zshrc-tmp"
-
 mkdir -p "$HOME/.config/exercism"
 cp "$HOME/.exercism-config/user.json" "$HOME/.config/exercism/user.json"
 exercism configure -w /workspaces/exercism
@@ -52,6 +30,28 @@ chmod +x linux-install-1.11.1.1347.sh
 sudo ./linux-install-1.11.1.1347.sh
 
 set +ex
+cat << EOF
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.'
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+EOF >"$HOME/.zshrc-tmp"
+
+cat "$HOME/.zshrc" >> "$HOME/.zshrc-tmp"
+
+cat << EOF
+
+# To customize prompt, run $(p10k configure) or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"
+
+EOF >>"$HOME/.zshrc-tmp"
+
+cp  "$HOME/.zshrc-tmp" "$HOME/.zshrc"
+rm "$HOME/.zshrc-tmp"
+
 source "$HOME/.zshrc"
 set -ex
 
--- lua/.devcontainer/postCreateCommand.sh
+++ lua/.devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 set -ex
 
@@ -7,12 +7,34 @@ echo -n 'export PATH=$HOME/.luarocks/bin:$PATH' >>~/.zshrc
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-set +ex
-source "$HOME/.zshrc"
-set -ex
-
 mkdir -p "$HOME/.config/exercism"
 cp "$HOME/.exercism-config/user.json" "$HOME/.config/exercism/user.json"
 exercism configure -w /workspaces/exercism
+
+set +ex
+cat << EOF
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.'
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+EOF >"$HOME/.zshrc-tmp"
+
+cat "$HOME/.zshrc" >> "$HOME/.zshrc-tmp"
+
+cat << EOF
+
+# To customize prompt, run $(p10k configure) or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"
+
+EOF >>"$HOME/.zshrc-tmp"
+
+cp  "$HOME/.zshrc-tmp" "$HOME/.zshrc"
+rm "$HOME/.zshrc-tmp"
+
+source "$HOME/.zshrc"
+set -ex
 
 omz theme set powerlevel10k/powerlevel10k
