8ce537a7aa8e8a4297d0a248fcb60d838fd40280
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2024-03-05 20:50:29 +0100

    Updating golang devcontainer

---
+++ go/.devcontainer/initializeCommand.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -e "$HOME/.ssh/id_rsa" ]; then
+    ssh-add "$HOME/.ssh/id_rsa"
+fi
+
+if [ -e "$HOME/.ssh/github_rsa" ]; then
+    ssh-add "$HOME/.ssh/github_rsa"
+fi
+
+if [ -e "$HOME/.ssh/arlm-GitHub" ]; then
+    ssh-add "$HOME/.ssh/arlm-GitHub"
+fi
+
+if [ -e "$HOME/.ssh/arlm-GitLab" ]; then
+    ssh-add "$HOME/.ssh/arlm-GitLab"
+fi
